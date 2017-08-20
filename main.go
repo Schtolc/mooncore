66a1fb1c8f96abe31c58f4c1aab8612bfa99eb4d
Victoria Suslova <suslova_vichka@mail.ru>
2017-08-20 19:39:13 +0300

    connect to db

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
     "net/http"
 
     "github.com/labstack/echo"
+	"time"
+	"fmt"
 )
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
+    db := ConnectDB()
+	fmt.Println("qweqwe")
+	e.GET("/", func(c echo.Context) error {
+
+		db.Create(&Product{Code: "200", Time: time.Now()})
         return c.String(http.StatusOK, "Hello, World!")
     })
-    e.Logger.Fatal(e.Start(":1323"))
+    e.Logger.Fatal(e.Start(":1325"))
+	fmt.Println("badbad")
+	defer db.Close()
 }
