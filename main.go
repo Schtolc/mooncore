8eec2ad11960f84876bc1bd9423546c023d7a671
Pavel <pavelgolubev345@gmail.com>
2017-08-20 16:15:44 +0300

    echo hello world [MCORE-1]

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+
+    "github.com/labstack/echo"
+)
 
 func main() {
-    fmt.Printf("test\n")
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!")
+    })
+    e.Logger.Fatal(e.Start(":1323"))
 }
