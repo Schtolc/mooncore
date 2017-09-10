08ad8e9afb973ad618e7208b6be93cb3f28dbc24
Alsaril <yakovlev1996@gmail.com>
2017-09-10 14:00:32 +0300

    models

--- models/model.go
+++ models/model.go
@@ -10,3 +10,54 @@ type Metric struct {
 	Path string `sql:"size:255;index"`
 	Time time.Time
 }
+
+type Address struct {
+	ID  int
+	Lat float32
+	Lon float32
+}
+
+type Photo struct {
+	ID   int
+	Path string
+}
+
+type User struct {
+	ID       int
+	Name     string
+	Email    string
+	Password string
+	Address  Address
+	Photo    Photo
+}
+
+type Master struct {
+	ID                 int
+	Name               string
+	Address            Address
+	Photo              Photo
+	WorkingPlacePhotos []Photo `gorm:"many2many:working_place_photos;"`
+}
+
+type Service struct {
+	ID                int
+	Name              string
+	Master            Master
+	Descr             string // TODO make unlimited
+	ManicureType      ManicureType
+	ManicureMaterials []ManicureMaterial `gorm:"many2many:service_manicure_materials;"`
+	Photos            []Photo            `gorm:"many2many:service_photos;"`
+}
+
+type ManicureType struct {
+	ID   int
+	Name string
+}
+
+type ManicureMaterial struct {
+	ID       int
+	Firm     string
+	Palette  string
+	Descr    string // TODO make unlimited
+	Services []Service `gorm:"many2many:service_manicure_materials;"`
+}
