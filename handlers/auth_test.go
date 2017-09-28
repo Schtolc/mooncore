78e92ce6b6feb75ca1010e9a2609c3e0ed7ffb01
Victoria Suslova <suslova_vichka@mail.ru>
2017-09-28 13:36:43 +0300

    auth tests[MCORE-10]

--- handlers/auth_test.go
+++ handlers/auth_test.go
@@ -13,28 +13,43 @@ var (
 		Password: "pass5",
 		Email:    "email5@mail.ru",
 	}
+	Stranger = &models.User{
+		Name:     "1",
+		Password: "1",
+		Email:    "1@mail.ru",
+	}
 	token string
 )
 
+
 func TestSignUp(t *testing.T) {
 	e := httpexpect.New(t, localhost.String())
-	m := map[string]string{
-		"Code": "200",
-	}
-	e.POST("/sign_up").WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().Equal(m)
 
+	e.POST("/sign_up").WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().Equal(emptyMessage)
+	e.POST("/sign_up").WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().Equal(userAlreadyExists)
+	e.POST("/sign_up").WithText("unexpected request body").Expect().Status(http.StatusOK).JSON().Object().Equal(internalError)
+
+	e.POST("/sign_up").WithJSON(&map[string]string{
+		"qwe": "qwe",
+	}).Expect().Status(http.StatusOK).JSON().Object().Equal(requireFields)
 }
+
+
 
 func TestSignIn(t *testing.T) {
 	e := httpexpect.New(t, localhost.String())
+
 	m := e.POST("/sign_in").WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().ContainsKey("Token")
 	token = m.Value("Token").String().Raw()
+
+	e.POST("/sign_in").WithJSON(Stranger).Expect().Status(http.StatusOK).JSON().Object().Equal(needRegistration)
 }
 
-func TestSignOut(t *testing.T) {
+func TestPingAuth(t *testing.T) {
 	e := httpexpect.New(t, localhost.String())
 	m := map[string]string{
-		"Code": "200",
+		"code": "200",
+		"message": "ECHO_AUTH_PING",
 	}
-	e.POST("/sign_out").WithHeader("Authorization", "Bearer "+token).WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().Equal(m)
+	e.POST("/auth_ping").WithHeader("Authorization", "Bearer "+token).WithJSON(testUser).Expect().Status(http.StatusOK).JSON().Object().Equal(m)
 }
