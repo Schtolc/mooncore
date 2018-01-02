f2468f269aeba550663dec21e4c34a9c0c605f74
Victoria Suslova <suslova_vichka@mail.ru>
2018-01-02 19:05:00 +0300

    change api place - remove big function add small files

--- handlers/api.go
+++ handlers/api.go
@@ -2,20 +2,30 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/Schtolc/mooncore/dependencies"
 	"github.com/Schtolc/mooncore/models"
 	"github.com/graphql-go/graphql"
-	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
-	"strconv"
 )
 
-func getUserDetails(email string, db *gorm.DB) (*models.UserDetails, error) {
+func just(param graphql.Type) *graphql.ArgumentConfig {
+	return &graphql.ArgumentConfig{
+		Type: param,
+	}
+}
+func notNull(param graphql.Type) *graphql.ArgumentConfig {
+	return just(graphql.NewNonNull(param))
+}
+func listof(param graphql.Type) *graphql.ArgumentConfig {
+	return just(graphql.NewList(param))
+}
+
+var db = dependencies.DBInstance()
+
+func getUserDetails(email string) (*models.UserDetails, error) {
 	user := &models.User{}
 	userDetails := &models.UserDetails{}
 	if dbc := db.Where("email in (?)", email).First(user); dbc.Error != nil {
@@ -29,460 +39,33 @@ func getUserDetails(email string, db *gorm.DB) (*models.UserDetails, error) {
 	return userDetails, nil
 }
 
-func getRootMutation(db *gorm.DB) *graphql.Object {
+func getRootMutation() *graphql.Object {
 	return graphql.NewObject(graphql.ObjectConfig{
 		Name: "RootMutation",
 		Fields: graphql.Fields{
-			"createUser": &graphql.Field{
-				Type:        UserObject,
-				Description: "Create new user",
-				Args: graphql.FieldConfigArgument{
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"password": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"role": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					tx := db.Begin()
-					user := &models.User{
-						Email:    params.Args["email"].(string),
-						Password: params.Args["password"].(string),
-						Role:     params.Args["role"].(int),
-					}
-					if err := tx.Create(user).Error; err != nil {
-						tx.Rollback()
-						return nil, err
-					}
-					userDetails := &models.UserDetails{
-						UserID:    user.ID,
-						AddressID: models.DefaultAddress.ID,
-						PhotoID:   models.DefaultAvatar.ID,
-					}
-					if err := tx.Create(userDetails).Error; err != nil {
-						tx.Rollback()
-						return nil, err
-					}
-					tx.Commit()
-					return user, nil
-				},
-			},
-			"editUserProfile": &graphql.Field{
-				Type:        UserDetailsObject,
-				Description: "Edit user fields",
-				Args: graphql.FieldConfigArgument{
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-					"name": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-					"address_id": &graphql.ArgumentConfig{
-						Type: graphql.Int,
-					},
-					"avatar_id": &graphql.ArgumentConfig{
-						Type: graphql.Int,
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Warn(err)
-						return nil, err
-					}
-					if params.Args["name"] != nil {
-						userDetails.Name = params.Args["name"].(string)
-					}
-					if params.Args["address_id"] != nil {
-						address := &models.Address{}
-						if dbc := db.Where("id = ?", params.Args["address_id"].(int)).First(address); dbc.Error != nil {
-							logrus.Error(dbc.Error)
-							return nil, dbc.Error
-						}
-						userDetails.AddressID = address.ID
-					}
-					if params.Args["photo"] != nil {
-						photo := &models.Photo{}
-						if dbc := db.Where("id = ?", params.Args["avatar_id"].(int)).First(photo); dbc.Error != nil {
-							logrus.Error(dbc.Error)
-							return nil, dbc.Error
-						}
-						userDetails.PhotoID = photo.ID
-					}
-					if dbc := db.Save(&userDetails); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return *userDetails, nil
-				},
-			},
-			"createAddress": &graphql.Field{
-				Type:        AddressObject,
-				Description: "Create new address",
-				Args: graphql.FieldConfigArgument{
-					"lat": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"lon": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"description": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					lat, err := strconv.ParseFloat(params.Args["lat"].(string), 64)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					lon, err := strconv.ParseFloat(params.Args["lon"].(string), 64)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					address := &models.Address{
-						Lat:         lat,
-						Lon:         lon,
-						Description: params.Args["description"].(string),
-					}
-					if dbc := db.Create(address); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return *address, nil
-				},
-			},
-			"createAvatar": &graphql.Field{
-				Type:        PhotoObject,
-				Description: "Create new address",
-				Args: graphql.FieldConfigArgument{
-					"path": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"tags": &graphql.ArgumentConfig{
-						Type: graphql.NewList(graphql.Int),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					photo := &models.Photo{
-						Path: params.Args["path"].(string),
-						Tags: []models.Tag{},
-					}
-					if dbc := db.Where("id in (?)", params.Args["tags"]).Find(&photo.Tags); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					if len(photo.Tags) == 0 && params.Args["tags"] != nil {
-						logrus.Error("No such tags")
-						return nil, errors.New("No such tags")
-					}
-
-					if dbc := db.Create(photo); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return *photo, nil
-				},
-			},
-			"addPhoto": &graphql.Field{
-				Type:        PhotoObject,
-				Description: "Add new photo - master",
-				Args: graphql.FieldConfigArgument{
-					"path": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"tags": &graphql.ArgumentConfig{
-						Type: graphql.NewList(graphql.Int),
-					},
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Warn(err)
-						return nil, err
-					}
-					photo := &models.Photo{
-						Path: params.Args["path"].(string),
-						Tags: []models.Tag{},
-					}
-					if dbc := db.Where("id in (?)", params.Args["tags"]).Find(&photo.Tags); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					if len(photo.Tags) == 0 && params.Args["tags"] != nil {
-						logrus.Error("No such tags")
-						return nil, errors.New("No such tags")
-					}
-					if dbc := db.Create(photo); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					if dbc := db.Model(&userDetails).Association("photos").Append([]models.Photo{*photo}); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return *photo, nil
-
-				},
-			},
-			"addSigns": &graphql.Field{
-				Type:        UserDetailsObject,
-				Description: "Add signs -  admin",
-				Args: graphql.FieldConfigArgument{
-					"signs": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.NewList(graphql.Int)),
-					},
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Warn(err)
-						return nil, err
-					}
-					signs := []models.Sign{}
-					if dbc := db.Where("id in (?)", params.Args["signs"]).Find(&signs); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					if len(signs) == 0 {
-						return nil, errors.New("No such signs")
-					}
-					tx := db.Begin()
-					for _, sign := range signs {
-						if err := tx.Model(&userDetails).Association("signs").Append([]models.Sign{sign}).Error; err != nil {
-							tx.Rollback()
-							return nil, err
-						}
-					}
-					tx.Commit()
-					return *userDetails, nil
-				},
-			},
+			"createUser":      createUser,
+			"editUserProfile": editUserProfile,
+			"createAddress":   createAddress,
+			"createAvatar":    createAvatar,
+			"addPhoto":        addPhoto,
+			"addSigns":        addSigns,
 		},
 	})
 }
 
-func getRootQuery(db *gorm.DB) *graphql.Object {
+func getRootQuery() *graphql.Object {
 	return graphql.NewObject(graphql.ObjectConfig{
 		Name: "RootQuery",
 		Fields: graphql.Fields{
-			"address": &graphql.Field{
-				Type:        AddressObject,
-				Description: "Get single address",
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					address := models.Address{}
-					if dbc := db.First(&address, params.Args["id"].(int)); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return address, nil
-				},
-			},
-			"addressList": &graphql.Field{
-				Type:        graphql.NewList(AddressObject),
-				Description: "List of addresses",
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					var addresses []models.Address
-					if dbc := db.Find(&addresses); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return addresses, nil
-				},
-			},
-			"addressListInArea": &graphql.Field{
-				Type:        graphql.NewList(AddressObject),
-				Description: "Get all addresses in this area",
-				Args: graphql.FieldConfigArgument{
-					"lat1": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"lon1": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"lat2": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"lon2": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					lat1, err := strconv.ParseFloat(params.Args["lat1"].(string), 64)
-					if err != nil {
-						logrus.Error(err) // первая точка сверху слева
-						return nil, err   // вторая снизу и справа
-					}
-					lon1, err := strconv.ParseFloat(params.Args["lon1"].(string), 64)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					lat2, err := strconv.ParseFloat(params.Args["lat2"].(string), 64)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					lon2, err := strconv.ParseFloat(params.Args["lon2"].(string), 64)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					var addresses []models.Address
-					query := "lat > ? AND lat < ? AND lon < ? AND lon > ?"
-					if dbc := db.Where(query, lat1, lat2, lon1, lon2).Find(&addresses); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return addresses, nil
-				},
-			},
-			"getPhoto": &graphql.Field{
-				Type:        PhotoObject,
-				Description: "Get single photo", // done
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					photo := &models.Photo{}
-
-					if dbc := db.First(&photo, params.Args["id"].(int)); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					if dbc := db.Model(&photo).Related(&photo.Tags, "Tags"); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return *photo, nil
-				},
-			},
-			"getUserPhotos": &graphql.Field{
-				Type:        graphql.NewList(PhotoObject),
-				Description: "Get single photo", //done
-				Args: graphql.FieldConfigArgument{
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Warn(err)
-						return nil, err
-					}
-					db.Model(&userDetails).Association("photos").Find(&userDetails.Photos)
-					return userDetails.Photos, nil
-				},
-			},
-			// done
-			"getUser": &graphql.Field{
-				Type:        UserDetailsObject,
-				Description: "Get single user",
-				Args: graphql.FieldConfigArgument{
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Error(err)
-						return nil, err
-					}
-					return *userDetails, nil
-				},
-			},
-			// done
-			"listUsers": &graphql.Field{
-				Type:        graphql.NewList(UserDetailsObject),
-				Description: "List of users",
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					var users []models.UserDetails
-					if dbc := db.Find(&users); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-					return users, nil
-				},
-			},
-			"getSigns": &graphql.Field{
-				Type:        graphql.NewList(SignObject),
-				Description: "Get user signs", //done
-				Args: graphql.FieldConfigArgument{
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					userDetails, err := getUserDetails(params.Args["email"].(string), db)
-					if err != nil {
-						logrus.Warn(err)
-						return nil, err
-					}
-					signs := []models.Sign{}
-					db.Model(&userDetails).Association("signs").Find(&signs)
-					return signs, nil
-				},
-			},
-			"getFeed": &graphql.Field{
-				Type:        graphql.NewList(UserDetailsObject),
-				Description: "feed",
-				Args: graphql.FieldConfigArgument{
-					"limit": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					var offset int
-					db.Table("user_details").Count(&offset)
-					offset = rand.Intn(offset)
-					users := []models.UserDetails{}
-
-					if dbc := db.Limit(p.Args["limit"].(int)).Offset(0).Find(&users); dbc.Error != nil {
-						logrus.Error(dbc.Error)
-						return nil, dbc.Error
-					}
-
-					for i, user := range users {
-						if dbc := db.Model(&user).Association("Photos").Find(&user.Photos); dbc.Error != nil {
-							logrus.Error(dbc.Error)
-							return nil, dbc.Error
-						}
-						for index, photo := range user.Photos {
-							if dbc := db.Model(&photo).Related(&photo.Tags, "Tags"); dbc.Error != nil {
-								logrus.Error(dbc.Error)
-								return nil, dbc.Error
-							}
-							user.Photos[index] = photo
-						}
-						if dbc := db.Model(&user).Association("Signs").Find(&user.Signs); dbc.Error != nil {
-							logrus.Error(dbc.Error)
-							return nil, dbc.Error
-						}
-						users[i] = user
-					}
-					return users, nil
-				},
-			},
+			"address":           address,
+			"addressList":       addressList,
+			"addressListInArea": addressListInArea,
+			"getPhoto":          getPhoto,
+			"getUserPhotos":     getUserPhotos,
+			"getUser":           getUser,
+			"listUsers":         listUsers,
+			"getSigns":          getSigns,
+			"feed":              feed,
 		},
 	})
 }
@@ -494,8 +77,8 @@ func createSchema() graphql.Schema {
 		return *schema
 	}
 	_schema, _ := graphql.NewSchema(graphql.SchemaConfig{
-		Query:    getRootQuery(dependencies.DBInstance()),
-		Mutation: getRootMutation(dependencies.DBInstance()),
+		Query:    getRootQuery(),
+		Mutation: getRootMutation(),
 	})
 	schema = &_schema
 	return *schema
---
+++ handlers/apiquery.go
@@ -0,0 +1,202 @@
+package handlers
+
+import (
+	"github.com/Schtolc/mooncore/models"
+	"github.com/graphql-go/graphql"
+	"github.com/sirupsen/logrus"
+	"math/rand"
+	"strconv"
+)
+
+var address = &graphql.Field{
+	Type:        AddressObject,
+	Description: "Get single address",
+	Args: graphql.FieldConfigArgument{
+		"id": notNull(graphql.Int),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		address := models.Address{}
+		if dbc := db.First(&address, params.Args["id"].(int)); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		return address, nil
+	},
+}
+
+var addressList = &graphql.Field{
+	Type:        graphql.NewList(AddressObject),
+	Description: "List of addresses",
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		var addresses []models.Address
+		if dbc := db.Find(&addresses); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		return addresses, nil
+	},
+}
+
+var addressListInArea = &graphql.Field{
+	Type:        graphql.NewList(AddressObject),
+	Description: "Get all addresses in this area",
+	Args: graphql.FieldConfigArgument{
+		"lat1": notNull(graphql.String),
+		"lon1": notNull(graphql.String),
+		"lat2": notNull(graphql.String),
+		"lon2": notNull(graphql.String),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		lat1, err := strconv.ParseFloat(params.Args["lat1"].(string), 64)
+		if err != nil {
+			logrus.Error(err) // первая точка сверху слева
+			return nil, err   // вторая снизу и справа
+		}
+		lon1, err := strconv.ParseFloat(params.Args["lon1"].(string), 64)
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+		lat2, err := strconv.ParseFloat(params.Args["lat2"].(string), 64)
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+		lon2, err := strconv.ParseFloat(params.Args["lon2"].(string), 64)
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+		var addresses []models.Address
+		query := "lat > ? AND lat < ? AND lon < ? AND lon > ?"
+		if dbc := db.Where(query, lat1, lat2, lon1, lon2).Find(&addresses); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		return addresses, nil
+	},
+}
+
+var getUserPhotos = &graphql.Field{
+	Type:        graphql.NewList(PhotoObject),
+	Description: "Get single photo", //done
+	Args: graphql.FieldConfigArgument{
+		"email": notNull(graphql.String),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		userDetails, err := getUserDetails(params.Args["email"].(string))
+		if err != nil {
+			logrus.Warn(err)
+			return nil, err
+		}
+		db.Model(&userDetails).Association("photos").Find(&userDetails.Photos)
+		return userDetails.Photos, nil
+	},
+}
+
+var getUser = &graphql.Field{
+	Type:        UserDetailsObject,
+	Description: "Get single user",
+	Args: graphql.FieldConfigArgument{
+		"email": notNull(graphql.String),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		userDetails, err := getUserDetails(params.Args["email"].(string))
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+		return *userDetails, nil
+	},
+}
+
+var listUsers = &graphql.Field{
+	Type:        graphql.NewList(UserDetailsObject),
+	Description: "List of users",
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		var users []models.UserDetails
+		if dbc := db.Find(&users); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		return users, nil
+	},
+}
+
+var getSigns = &graphql.Field{
+	Type:        graphql.NewList(SignObject),
+	Description: "Get user signs", //done
+	Args: graphql.FieldConfigArgument{
+		"email": notNull(graphql.String),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		userDetails, err := getUserDetails(params.Args["email"].(string))
+		if err != nil {
+			logrus.Warn(err)
+			return nil, err
+		}
+		signs := []models.Sign{}
+		db.Model(&userDetails).Association("signs").Find(&signs)
+		return signs, nil
+	},
+}
+
+var feed = &graphql.Field{
+	Type:        graphql.NewList(UserDetailsObject),
+	Description: "feed",
+	Args: graphql.FieldConfigArgument{
+		"limit": notNull(graphql.Int),
+	},
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		var offset int
+		db.Table("user_details").Count(&offset)
+		offset = rand.Intn(offset)
+		users := []models.UserDetails{}
+
+		if dbc := db.Limit(p.Args["limit"].(int)).Offset(0).Find(&users); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+
+		for i, user := range users {
+			if dbc := db.Model(&user).Association("Photos").Find(&user.Photos); dbc.Error != nil {
+				logrus.Error(dbc.Error)
+				return nil, dbc.Error
+			}
+			for index, photo := range user.Photos {
+				if dbc := db.Model(&photo).Related(&photo.Tags, "Tags"); dbc.Error != nil {
+					logrus.Error(dbc.Error)
+					return nil, dbc.Error
+				}
+				user.Photos[index] = photo
+			}
+			if dbc := db.Model(&user).Association("Signs").Find(&user.Signs); dbc.Error != nil {
+				logrus.Error(dbc.Error)
+				return nil, dbc.Error
+			}
+			users[i] = user
+		}
+		return users, nil
+	},
+}
+
+var getPhoto = &graphql.Field{
+	Type:        PhotoObject,
+	Description: "Get single photo", // done
+	Args: graphql.FieldConfigArgument{
+		"id": notNull(graphql.Int),
+	},
+	Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+		photo := &models.Photo{}
+
+		if dbc := db.First(&photo, params.Args["id"].(int)); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		if dbc := db.Model(&photo).Related(&photo.Tags, "Tags"); dbc.Error != nil {
+			logrus.Error(dbc.Error)
+			return nil, dbc.Error
+		}
+		return *photo, nil
+	},
+}
